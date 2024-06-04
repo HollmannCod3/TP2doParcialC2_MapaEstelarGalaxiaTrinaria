8abe8a4a67bd870382be864a6b86ccc1c1347e53
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-06-04 19:21:08 -0300

    Suba de Proyecto NetBeans

---
+++ GalaxiaTernaria_EDAtp2.java
@@ -0,0 +1,17 @@
+
+package galaxiaternaria_edatp2;
+
+/**
+ *
+ * @author Hollmann
+ */
+public class GalaxiaTernaria_EDAtp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
