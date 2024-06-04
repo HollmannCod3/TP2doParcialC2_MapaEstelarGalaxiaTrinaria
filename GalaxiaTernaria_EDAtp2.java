5a632455dae04f4cab86c6a684691ad24535e7bb
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-06-04 20:59:23 -0300

    "avances"

--- GalaxiaTernaria_EDAtp2.java
+++ GalaxiaTernaria_EDAtp2.java
@@ -12,6 +12,24 @@ public class GalaxiaTernaria_EDAtp2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArbolTernario galaxia = new ArbolTernario();
+        
+        galaxia.insertar("nico", " ", 0);
+        galaxia.insertar("cristian", "nico", 0);
+        galaxia.insertar("a", "nico ", 1);
+        galaxia.insertar("b", "nico ", 2);
+        galaxia.insertar("c", "cristian", 0);
+        galaxia.insertar("d", "cristian", 1);
+        galaxia.insertar("e", "cristian", 2);
+        galaxia.insertar("nico", "a", 0);
+        galaxia.insertar("nico", "a", 1);
+        galaxia.insertar("nico", "a", 2);
+        galaxia.insertar("nico", "b", 0);
+        galaxia.insertar("nico", "b", 1);
+        galaxia.insertar("nico", "b", 2);
+        
+        System.out.println(galaxia);
+        
     }
     
 }
